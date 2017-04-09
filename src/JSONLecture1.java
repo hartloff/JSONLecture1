2ff753cb5c834dbe541316ee6f1ea8cdbce20661
jessehartloff <hartloff@buffalo.edu>
2017-04-09 15:31:42 -0400

    initial commit

---
+++ src/JSONLecture1.java
@@ -0,0 +1,18 @@
+
+public class JSONLecture1 {
+
+
+    // 5 points
+    static String Q1(String nameValue, String titleValue, int rankValue) {
+        // return a String in the format of a JSON object containing three key-value pairs where the keys
+        // are "name", "title", and "rank" and values are provided as parameters of this method.
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
